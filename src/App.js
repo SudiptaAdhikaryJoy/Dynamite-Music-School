75ec9c4f0b8ccdb09b58f62239f7862d3fad027e
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-10-04 02:47:38 +0600

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Services from './components/Services/Services';
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+
+          <Route path="/about">
+            <About></About>
+          </Route>
+
+          <Route path="/service">
+            <Services></Services>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
