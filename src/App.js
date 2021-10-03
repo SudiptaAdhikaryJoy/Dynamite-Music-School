b6ab4456bbf26b5e4d03c8e5594cb85ed092c16c
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-10-04 03:09:52 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/Services/Services';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div>
+    <div className="App">
       <Router>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-
-          <Route path="/about">
-            <About></About>
-          </Route>
-
-          <Route path="/service">
-            <Services></Services>
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+      </Switch>
       </Router>
     </div>
   );
