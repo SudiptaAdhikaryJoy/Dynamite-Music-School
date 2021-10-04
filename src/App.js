82ef27b35befc3b7560c4528d4f893cdb3a08735
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-10-04 12:36:05 +0600

    add json database'

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header></Header>
       <Switch>
         <Route path="/home">
           <Home></Home>
         </Route>
       </Switch>
+      <Footer></Footer>
       </Router>
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+            <h2>hello</h2>
         </div>
     );
 };
