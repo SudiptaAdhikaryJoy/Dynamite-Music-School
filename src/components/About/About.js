68dc609c7e6295702c672dc679ed5b2cea6626a8
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-10-05 21:05:45 +0600

    add styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,17 @@ import Home from '../Home/Home';
 const About = () => {
     return (
         <div>
-            <h2>hello world</h2>
-            <Home></Home>
+            <div className="mt-5">
+                <div className="d-flex p-1">
+                    <div>
+                        <img className="img-fluid w-50 rounded" src="https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bXVzaWMlMjBzY2hvb2x8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" alt="" />
+                    </div>
+                    <div className="text-start">
+                        <h1>Music Office</h1>
+                        <p>Happy productive employees mean a successful business. We provide workplace music tuition and experiences to promote wellbeing and collaboration.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
